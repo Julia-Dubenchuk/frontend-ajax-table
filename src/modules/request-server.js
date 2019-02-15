83e09d6770eb2d60974dc3f7da0092ecb09a53cd
dubenchuk6@gmail.com <dubenchuk6@gmail.com>
2019-02-15 11:36:42 +0200

    change name arguments in function request

--- src/modules/request-server.js
+++ src/modules/request-server.js
@@ -3,12 +3,12 @@ import axios from 'axios';
 import { filter } from './filter';
 import { outputTable } from './output-table';
 
-export function requestServer (typeTable, func = filter, filt) {
+export function requestServer (typeTable, filterCallback = filter, dataCallback) {
 	const URL = 'https://www.mocky.io/v2/55f748b33568195d044b3dc8';
 
 	axios.get(URL)
 		.then(({ data }) => {
-			outputTable(func(data, filt), typeTable);
+			outputTable(filterCallback(data, dataCallback), typeTable);
 			return;
 		})
 		.catch(error => new Error(`Network Error ${error}`));
